8633f8899c9fed6b5825281aab99df6867f53b2e
choccho <shocho0101@hotmail.co.jp>
2019-11-03 20:45:33 +0900

    make advanced part

--- app/src/main/java/com/example/profile/MainActivity.kt
+++ app/src/main/java/com/example/profile/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.profile
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,12 +12,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        myImageView.visibility = View.INVISIBLE
-        myText.visibility = View.INVISIBLE
-
         myNameButton.setOnClickListener {
-            myImageView.visibility = View.VISIBLE
-            myText.visibility = View.VISIBLE
+            myNameButton.setBackgroundColor(Color.parseColor("#29abe2"))
+            myLikeSportsButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeFoodsButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeHobbyButton.setBackgroundColor(Color.parseColor("#929292"))
+            myImageView.setImageResource(R.drawable.tobisuke)
+            myText.text = "とびすけ"
         }
+
+        myLikeSportsButton.setOnClickListener {
+            myLikeSportsButton.setBackgroundColor(Color.parseColor("#29abe2"))
+            myNameButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeFoodsButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeHobbyButton.setBackgroundColor(Color.parseColor("#929292"))
+            myImageView.setImageResource(R.drawable.soccer)
+            myText.text = "サッカー"
+        }
+
+        myLikeFoodsButton.setOnClickListener {
+            myLikeFoodsButton.setBackgroundColor(Color.parseColor("#29abe2"))
+            myNameButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeSportsButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeHobbyButton.setBackgroundColor(Color.parseColor("#929292"))
+            myImageView.setImageResource(R.drawable.sushi)
+            myText.text = "すし"
+        }
+
+        myLikeHobbyButton.setOnClickListener {
+            myLikeHobbyButton.setBackgroundColor(Color.parseColor("#29abe2"))
+            myNameButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeSportsButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeFoodsButton.setBackgroundColor(Color.parseColor("#929292"))
+            myImageView.setImageResource(R.drawable.dance)
+            myText.text = "ダンス"
+        }
+
+
+
+
+//        myImageView.visibility = View.INVISIBLE
+//        myText.visibility = View.INVISIBLE
+//
+//        myNameButton.setOnClickListener {
+//            myImageView.visibility = View.VISIBLE
+//            myText.visibility = View.VISIBLE
+//        }
     }
 }
